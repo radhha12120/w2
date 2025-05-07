d1e3e3c3248d8a65191454c8d5d85a9c1f6bd4dc
radhha12120 <radhha12120@gmail.com>
2025-05-08 01:00:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// // script.js
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const form = document.getElementById("registrationForm");
-//     if (form) {
-//       form.addEventListener("submit", function (e) {
-//         e.preventDefault();
-  
-//         const name = document.getElementById("name").value;
-//         const email = document.getElementById("email").value;
-//         const password = document.getElementById("password").value;
-  
-//         const userData = { name, email, password };
-  
-//         // Simulate AJAX POST (here just pushing to localStorage)
-//         let users = JSON.parse(localStorage.getItem("users")) || [];
-//         users.push(userData);
-//         localStorage.setItem("users", JSON.stringify(users));
-  
-//         alert("User registered successfully!");
-  
-//         form.reset();
-        
-//       });
-//     }
-//   });
-  
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registrationForm");
